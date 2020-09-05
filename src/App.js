e6d8dbb51a8838e33d58d95e318bf35a7d4d175a
saadmanahmed <46515754+saadmanahmed@users.noreply.github.com>
2020-09-06 02:09:02 +0800

    First Commit-Basic Layout of an Appointment App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
+import React,{Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import AddAppointments from './AddApointments'
+import ListAppointments from './ListAppointments'
+import SearchAppointments from './SearchAppointments'
+import { without, last } from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+    
+      myAppointments:[],
+      lastIndex:0,
+      formDisplay:false,
+      orderBy:'ownerName',
+      orderDir:'asc',
+
+    }
+    this.deleteAppointment=this.deleteAppointment.bind(this);
+    this.toggleForm=this.toggleForm.bind(this);
+    this.addAppointment=this.addAppointment.bind(this);
+  }
+
+  toggleForm(){
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    }
+    )
+  }
+
+  deleteAppointment(apt){
+   let tempApt=this.state.myAppointments;
+   tempApt=without(tempApt,apt);
+
+   this.setState({
+     myAppointments:tempApt
+   })
+
 }
+addAppointment(apt){
+  let tempApt=this.state.myAppointments;
+  apt.Id=this.state.lastIndex;
+  tempApt.unshift(apt);
+  this.setState({
+    myAppointments:tempApt,
+    lastIndex:this.state.lastIndex+1
+  })
+}
+  componentDidMount(){
+    fetch('./data.json').then(response=>response.json()).then(result=>{const apts=result.map(e=>{
+    e.Id=this.state.lastIndex;
+    this.setState({lastIndex:this.state.lastIndex+1})
+
+    
+      return e;
+    })
+    this.setState(
+      {
+        myAppointments:apts
+      }
+    )
+  });
+    
+  }
+ 
+
+
+
+  render() {
+
+    let order;
+    let filteredApts=this.state.myAppointments;
+    if(this.state.orderDir==='asc'){
+      order=1;
+    }
+    else{order=-1};
+
+    filteredApts.sort((a,b)=>{
+    if(a[this.state.orderBy].toLowerCase()<b[this.state.orderBy].toLowerCase()){
+      return -1;
+    }
+    else{
+      return 1;
+    }
+  }
+    
+    
+    )
+
+    
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                 
+                 <AddAppointments addAppointment={this.addAppointment} formDisplay={this.state.formDisplay} toggleForm={this.toggleForm}/>
+                 <ListAppointments appointments={filteredApts} deleteAppointment={this.deleteAppointment}/>
+                 <SearchAppointments/>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
 
 export default App;
