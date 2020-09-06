52748ee11fcf539c99728241dd76fe54b4a0840a
saadmanahmed <46515754+saadmanahmed@users.noreply.github.com>
2020-09-07 00:28:29 +0800

    Used prettier to improve code readability

--- src/App.js
+++ src/App.js
@@ -1,124 +1,121 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
-import AddAppointments from './AddApointments'
-import ListAppointments from './ListAppointments'
-import SearchAppointments from './SearchAppointments'
-import { without, last } from 'lodash';
-
+import logo from "./logo.svg";
+import "./App.css";
+import AddAppointments from "./AddApointments";
+import ListAppointments from "./ListAppointments";
+import SearchAppointments from "./SearchAppointments";
+import { without, last } from "lodash";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-    
-      myAppointments:[],
-      lastIndex:0,
-      formDisplay:false,
-      orderBy:'ownerName',
-      orderDir:'asc',
-      queryText:'',
-
-    }
-    this.deleteAppointment=this.deleteAppointment.bind(this);
-    this.toggleForm=this.toggleForm.bind(this);
-    this.addAppointment=this.addAppointment.bind(this);
-    this.searchApts=this.searchApts.bind(this);
+    this.state = {
+      myAppointments: [],
+      lastIndex: 0,
+      formDisplay: false,
+      orderBy: "ownerName",
+      orderDir: "asc",
+      queryText: "",
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.searchApts = this.searchApts.bind(this);
   }
 
-  toggleForm(){
+  toggleForm() {
     this.setState({
-      formDisplay: !this.state.formDisplay
-    }
-    )
+      formDisplay: !this.state.formDisplay,
+    });
   }
 
-  deleteAppointment(apt){
-   let tempApt=this.state.myAppointments;
-   tempApt=without(tempApt,apt);
+  deleteAppointment(apt) {
+    let tempApt = this.state.myAppointments;
+    tempApt = without(tempApt, apt);
 
-   this.setState({
-     myAppointments:tempApt
-   })
-
-}
-addAppointment(apt){
-  let tempApt=this.state.myAppointments;
-  apt.Id=this.state.lastIndex;
-  tempApt.unshift(apt);
-  this.setState({
-    myAppointments:tempApt,
-    lastIndex:this.state.lastIndex+1
-  })
-}
-
-searchApts(query){
-  this.setState(
-    {
-      queryText:query
-    }
-  )
- 
-}
-  componentDidMount(){
-    fetch('./data.json').then(response=>response.json()).then(result=>{const apts=result.map(e=>{
-    e.Id=this.state.lastIndex;
-    this.setState({lastIndex:this.state.lastIndex+1})
-
-    
-      return e;
-    })
-    this.setState(
-      {
-        myAppointments:apts
-      }
-    )
-  });
-    
+    this.setState({
+      myAppointments: tempApt,
+    });
   }
- 
+  addAppointment(apt) {
+    let tempApt = this.state.myAppointments;
+    apt.Id = this.state.lastIndex;
+    tempApt.unshift(apt);
+    this.setState({
+      myAppointments: tempApt,
+      lastIndex: this.state.lastIndex + 1,
+    });
+  }
 
+  searchApts(query) {
+    this.setState({
+      queryText: query,
+    });
+  }
+  componentDidMount() {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((result) => {
+        const apts = result.map((e) => {
+          e.Id = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
 
+          return e;
+        });
+        this.setState({
+          myAppointments: apts,
+        });
+      });
+  }
 
   render() {
-
     let order;
-    let filteredApts=this.state.myAppointments;
-    if(this.state.orderDir==='asc'){
-      order=1;
+    let filteredApts = this.state.myAppointments;
+    if (this.state.orderDir === "asc") {
+      order = 1;
+    } else {
+      order = -1;
     }
-    else{order=-1};
 
-    filteredApts=filteredApts.sort((a,b)=>{
-    if(a[this.state.orderBy].toLowerCase()<b[this.state.orderBy].toLowerCase()){
-      return -1;
-    }
-    else{
-      return 1;
-    }
-  }
-    
-    
-    ).filter(eachItem=>{
-      return(
-        eachItem['petName'].toLowerCase().includes(this.state.queryText.toLowerCase())||
-        eachItem['ownerName'].toLowerCase().includes(this.state.queryText.toLowerCase())
-      )
-    })
+    filteredApts = filteredApts
+      .sort((a, b) => {
+        if (
+          a[this.state.orderBy].toLowerCase() <
+          b[this.state.orderBy].toLowerCase()
+        ) {
+          return -1;
+        } else {
+          return 1;
+        }
+      })
+      .filter((eachItem) => {
+        return (
+          eachItem["petName"]
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          eachItem["ownerName"]
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase())
+        );
+      });
 
-    
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                 
-                 <AddAppointments addAppointment={this.addAppointment} formDisplay={this.state.formDisplay} toggleForm={this.toggleForm}/>
-                 <ListAppointments appointments={filteredApts} deleteAppointment={this.deleteAppointment}/>
-                 <SearchAppointments searchApts={this.searchApts}/>
-                
+                <AddAppointments
+                  addAppointment={this.addAppointment}
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                />
+                <ListAppointments
+                  appointments={filteredApts}
+                  deleteAppointment={this.deleteAppointment}
+                />
+                <SearchAppointments searchApts={this.searchApts} />
               </div>
             </div>
           </div>
@@ -127,6 +124,5 @@ searchApts(query){
     );
   }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'jquery/dist/jquery.js';
-import 'popper.js/dist/umd/popper.js';
-import 'bootstrap/dist/js/bootstrap.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "jquery/dist/jquery.js";
+import "popper.js/dist/umd/popper.js";
+import "bootstrap/dist/js/bootstrap.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
